653280d7f85b09956886efde5659764efc341af8
ianpatricck <ianpatrick499@gmail.com>
2022-04-23 10:56:25 -0300

    range 1-10 from english words

--- src/cases/GenerateRandomText.ts
+++ src/cases/GenerateRandomText.ts
@@ -5,9 +5,9 @@ class GenerateRandomText {
 
     static async getEnglishText(req: Request, res: Response): Promise<void> {
 
-        const id: number = Math.floor(Math.random() * 20) + 1;
+        const id: number = Math.floor(Math.random() * 10) + 1;
 
-        await client.query('SELECT text FROM english WHERE id = $1', [id]).then((textData) => {
+        await client.query("SELECT text FROM english WHERE id = $1", [id]).then((textData) => {
 
             const text = textData.rows[0].text;
              
