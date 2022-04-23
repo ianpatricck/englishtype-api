44b83bdb3acd73bf774ab988c2ab5bf3ad1311dc
ianpatricck <ianpatrick499@gmail.com>
2022-04-22 22:00:52 -0300

    DB_HOST env var

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const client = new Client({
     user: process.env.DB_USERNAME,
-    host: process.env.APP_HOST,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: 5432 || process.env.DB_PORT
